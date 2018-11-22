22a4a5b55c0934fded8ced9111d5fcd16c5742e2
jpakulski01 <43757444+jpakulski01@users.noreply.github.com>
2018-11-21 23:34:42 -0500

    Update problem-set-4.js

--- js/problem-set-4.js
+++ js/problem-set-4.js
@@ -5,7 +5,8 @@
 function hello() {
 
   // WRITE YOUR EXERCISE 1 CODE HERE
-
+let div = document.getElementById("output1");
+div.innerHTML = "Hello, AP Computer Science Principles!";
   ////////////////// DO NOT MODIFY
   check("hello"); // DO NOT MODIFY
   ////////////////// DO NOT MODIFY
@@ -21,11 +22,13 @@ function helloAgain() {
   let name; // DO NOT MODIFY
   //////////// DO NOT MODIFY
 
-  // Use the name variable declared above to store the user's response. You
+  // Use the name letiable declared above to store the user's response. You
   // do not need to re-declare it, only assign it a value.
 
   // WRITE YOUR EXERCISE 2 CODE HERE
-
+  name = prompt("Please Enter Your Name");
+  let div2 = document.getElementById("output2");
+  div2.innerHTML = "Hello, "+ name + "!";
   ///////////////////////////// DO NOT MODIFY
   check("helloAgain", name); // DO NOT MODIFY
   ///////////////////////////// DO NOT MODIFY
@@ -45,7 +48,9 @@ function celsius() {
   // (inclusive), and rounds this value to 2 decimal places.
 
   // WRITE YOUR EXERCISE 3 CODE HERE
-
+  let fahrenheit  = ((cels * 9/5)+32).toFixed(2);
+  let div3 = document.getElementById("output3");
+  div3.innerHTML = (cels + " degrees Celsius equals " + fahrenheit + " degrees Fahrenheit.");
   ////////////////////////// DO NOT MODIFY
   check("celsius", cels); // DO NOT MODIFY
   ////////////////////////// DO NOT MODIFY
@@ -65,7 +70,9 @@ function fahrenheit() {
   // (inclusive), and rounds this value to 2 decimal places.
 
   // WRITE YOUR EXERCISE 4 CODE HERE
-
+  let celsius =  ((fahr-32)*(5/9)).toFixed(2);
+  let div4 = document.getElementById("output4");
+  div4.innerHTML = (fahr + " degrees Fahrenheit equals " + celsius + " degrees Celsius.");
   ///////////////////////////// DO NOT MODIFY
   check("fahrenheit", fahr); // DO NOT MODIFY
   ///////////////////////////// DO NOT MODIFY
@@ -87,7 +94,12 @@ function inches() {
   ////////////////////// DO NOT MODIFY
   let inches = input; // DO NOT MODIFY
   ////////////////////// DO NOT MODIFY
-
+  let miles = Math.floor(inches/63360);
+  let yards = Math.floor((inches - (miles*63360))/36);
+  let feet = Math.floor((inches - (miles*63360) - (yards*36))/12);
+  inches = Math.floor((inches - (miles*63360) - (yards*36) - (feet*12))/1);
+  let div5 = document.getElementById("output5");
+  div5.innerHTML = ("Miles: " + miles + "<br/>Yards: " + yards + "<br/>Feet: " + feet + "<br/>Inches: " + inches);
   // WRITE YOUR EXERCISE 5 CODE HERE
 
   ////////////////////////// DO NOT MODIFY
@@ -113,7 +125,11 @@ function centimeters() {
   /////////////////////////// DO NOT MODIFY
 
   // WRITE YOUR EXERCISE 6 CODE HERE
-
+  let kilometers = Math.floor(centimeters/100000);
+  let meters = Math.floor((centimeters - (kilometers*100000))/100);
+  centimeters = Math.floor((centimeters - (kilometers*100000) - (meters*100))/1);
+  let div6 = document.getElementById("output6");
+  div6.innerHTML = ("Kilometers: " + kilometers +"</br>Meters: " + meters +"</br>Centimeters: " + centimeters);
   /////////////////////////////// DO NOT MODIFY
   check("centimeters", input); // DO NOT MODIFY
   /////////////////////////////// DO NOT MODIFY
@@ -137,7 +153,13 @@ function fluidOunces() {
   /////////////////////////// DO NOT MODIFY
 
   // WRITE YOUR EXERCISE 7 CODE HERE
-
+  let gallons = Math.floor(fluidOunces/128);
+  let quarts = Math.floor((fluidOunces - (gallons*128))/32);
+  let pints = Math.floor((fluidOunces - (gallons*128) - (quarts*32))/16);
+  let cups = Math.floor((fluidOunces - (gallons*128) - (quarts*32) - (pints*16))/8);
+  fluidOunces = Math.floor((fluidOunces - (gallons*128) - (quarts*32) - (pints*16) - (cups*8))/1);
+  let div7 = document.getElementById("output7");
+  div7.innerHTML = ("Gallons: " + gallons + "</br>Quarts: " + quarts + "</br>Pints: " + pints + "</br>Cups: " + cups + "</br>Fluid Ounces: " + fluidOunces);
   /////////////////////////////// DO NOT MODIFY
   check("fluidOunces", input); // DO NOT MODIFY
   /////////////////////////////// DO NOT MODIFY
@@ -161,7 +183,11 @@ function ounces() {
   ////////////////////// DO NOT MODIFY
 
   // WRITE YOUR EXERCISE 8 CODE HERE
-
+  let tons = Math.floor(ounces/32000);
+  let pounds = Math.floor((ounces - (tons*32000))/16);
+  ounces = Math.floor((ounces - (tons*32000) - (pounds*16))/1);
+  let div8 = document.getElementById("output8")
+  div8.innerHTML = ("Tons: " + tons + "</br>Pounds: " + pounds + "</br>Ounces: " + ounces);
   ////////////////////////// DO NOT MODIFY
   check("ounces", input); // DO NOT MODIFY
   ////////////////////////// DO NOT MODIFY
@@ -185,7 +211,13 @@ function money() {
   /////////////////////// DO NOT MODIFY
 
   // WRITE YOUR EXERCISE 9 CODE HERE
-
+  let dollars = Math.floor(pennies/100);
+  let quarters = Math.floor((pennies - (dollars*100))/25);
+  let dimes = Math.floor((pennies - (dollars*100) - (quarters*25))/10);
+  let nickels = Math.floor((pennies - (dollars*100) - (quarters*25) - (dimes*10))/5);
+  let div9 = document.getElementById("output9")
+  pennies = Math.floor((pennies - (dollars*100) - (quarters*25) - (dimes*10) - (nickels*5))/1);
+  div9.innerHTML = ("Dollars: " + dollars + "<br/>Quarters: " + quarters + "<br/>Dimes: " + dimes + "<br/>Nickels: " + nickels + "<br/>Pennies: " + pennies)
   ///////////////////////// DO NOT MODIFY
   check("money", input); // DO NOT MODIFY
   ///////////////////////// DO NOT MODIFY
@@ -209,7 +241,13 @@ function change() {
   ////////////////////// DO NOT MODIFY
 
   // WRITE YOUR EXERCISE 10 CODE HERE
-
+  amount*=100
+  let quarters=Math.floor((amount)/25);
+  let dimes=Math.floor((amount - (quarters*25))/10);
+  let nickels=Math.floor((amount - (quarters*25) - (dimes*10))/5);
+  let pennies=Math.floor((amount - (quarters*25) - (dimes*10) - (nickels*5))/1);
+  let div10=document.getElementById("output10")
+  div10.innerHTML=(quarters + dimes + nickels + pennies + " coins.")
   ////////////////////////// DO NOT MODIFY
   check("change", input); // DO NOT MODIFY
   ////////////////////////// DO NOT MODIFY
